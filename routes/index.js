e4c88b22a17f84c92d884dfece2c4f4b47d0cb8f
HotG8119 <k508060@gmail.com>
2023-08-27 16:02:54 +0800

    feat: flash msg & signup verification

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const admin = require('./modules/admin')
 const classController = require('../controllers/class-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
 
@@ -12,5 +13,6 @@ router.post('/signup', userController.signUp)
 router.get('/classes', classController.getClasses)
 
 router.use('/', (req, res) => res.redirect('/classes'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
