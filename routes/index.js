ec2100e3c6447d12be2e4ca2ef29de764679cdd7
HotG8119 <k508060@gmail.com>
2023-08-27 15:41:56 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,12 @@ const router = express.Router()
 
 const admin = require('./modules/admin')
 const classController = require('../controllers/class-controller')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
 
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/classes', classController.getClasses)
 
 router.use('/', (req, res) => res.redirect('/classes'))
