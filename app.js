af617aaf71ec29b13cecbbd847206cdd88249447
HotG8119 <k508060@gmail.com>
2023-08-28 20:43:15 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
+const { getUser } = require('./helpers/auth-helpers')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
 const routes = require('./routes')
 const SESSION_SECRET = 'secret'
@@ -10,7 +12,7 @@ const SESSION_SECRET = 'secret'
 const app = express()
 const port = process.env.PORT || 3000
 
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 
@@ -21,13 +23,14 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
   next()
 })
 
 app.use(routes)
 
 app.listen(port, () => {
-  console.info(`Example app listening on port ${port}!`)
+  console.info(`Example app listening on http://localhost:${port} !`)
 })
 
 module.exports = app
