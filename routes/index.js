fa433bfc5188d74bbdd732f3560bf4a5d07c4bb6
HotG8119 <k508060@gmail.com>
2023-08-29 01:27:17 +0800

    feat: add user authenticated feature

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const passport = require('../config/passport')
 const admin = require('./modules/admin')
 const classController = require('../controllers/class-controller')
 const userController = require('../controllers/user-controller')
+const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
@@ -12,10 +13,10 @@ router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
-router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
 
-router.get('/classes', classController.getClasses)
+router.get('/classes', authenticated, classController.getClasses)
 
 router.use('/', (req, res) => res.redirect('/classes'))
 router.use('/', generalErrorHandler)
