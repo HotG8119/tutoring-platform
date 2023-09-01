a5bfd3a153afd8d016e3ec62863f93b5c9beaaf8
HotG8119 <k508060@gmail.com>
2023-09-01 19:44:01 +0800

    feat: add user seed

---
+++ seeders/20230901092750-users-seed-file.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [{
+      name: 'user1',
+      email: 'user1@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      introduce: 'I am user1',
+      image: 'https://i.imgur.com/hepj9ZS.png',
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      name: 'user2',
+      email: 'user2@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      introduce: 'I am user2',
+      image: 'https://i.imgur.com/hepj9ZS.png',
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      name: 'user3',
+      email: 'user3@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      introduce: 'I am user3',
+      image: 'https://i.imgur.com/hepj9ZS.png',
+      created_at: new Date(),
+      updated_at: new Date()
+    }])
+
+    await queryInterface.bulkInsert('Users',
+      Array.from({ length: 20 }).map(() => ({
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(8),
+        image: 'https://i.imgur.com/hepj9ZS.png',
+        introduce: faker.lorem.text().substring(0, 80),
+        created_at: new Date(),
+        updated_at: new Date()
+      })
+      ))
+  },
+  down: async (queryInterface, Sequelize) => { // 清空資料表中所有資料
+    await queryInterface.bulkDelete('Users', {})
+  }
+}
