a2088f5f97c93b5e5bee25f4e908a6daf35009c9
HotG8119 <k508060@gmail.com>
2023-09-09 12:16:21 +0800

    feat: add admin main page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,8 @@
-const adminController = {
+const { User, TeacherInfo } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
+const dayjs = require('dayjs')
 
+const adminController = {
   signInPage: (req, res) => {
     return res.render('admin/signin')
   },
@@ -13,7 +16,32 @@ const adminController = {
     res.redirect('/admin/signin')
   },
   getUsers: (req, res) => {
-    return res.render('admin/users')
+    const DEFAULT_LIMIT = 9
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
+    User.findAndCountAll({
+      raw: true,
+      nest: true,
+      attributes: { exclude: ['password'] },
+      include: {
+        model: TeacherInfo,
+        attributes: ['id', 'classIntroduce', 'method', 'availableWeekdays']
+      },
+      limit,
+      offset
+    }).then(users => {
+      const result = users.rows.map(user => ({
+        ...user,
+        createdAt: dayjs(user.createdAt).format('YYYY-MM-DD'),
+        // 將availableWeekdays解析並排序
+        availableWeekdays: user.TeacherInfo.availableWeekdays
+          ? JSON.parse(user.TeacherInfo.availableWeekdays).sort((a, b) => a - b)
+          : null
+      }))
+      return res.render('admin/users', { result, pagination: getPagination(limit, page, users.count) })
+    })
   }
 }
 
